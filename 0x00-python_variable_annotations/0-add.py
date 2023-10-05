816e0ee25ec84abdf21d7dd3a5fca934355677f9
mideactive <jenyoolumide@yahoo.com>
2023-10-05 09:24:18 +0000

    Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Task 0: Basic annotation - add """
+
+
+def add(a: float, b: float) -> float:
+    """ add two floating-point numbers
+    and return the result as float """
+    return a + b
